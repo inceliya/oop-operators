6d6f04a4be1048d2c5ffaee3c20d5e1f13cc96b5
Maksim Zakharchuk <zahar2002263802@gmail.com>
2019-11-30 14:25:48 +0200

    1/2

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Fraction
+    {
+        protected long Chis;
+        protected long Znam;
+
+        public Fraction(long chis, long znam = 1)
+        {
+            Chis = chis;
+            Znam = znam;
+            Fix();
+            Cut();
+        }
+        public Fraction(double chis)
+        {
+            long znam = 1;
+            while (chis != Convert.ToInt32(chis))
+            {
+                chis *= 10;
+                znam *= 10;
+            }
+            Chis = Convert.ToInt32(chis);
+            Znam = znam;
+            Fix();
+            Cut();
+        }
+
+        public void Cut()
+        {
+            int max, min;
+            if (Chis == Znam)
+            {
+                Chis = 1;
+                Znam = 1;
+            }
+            else
+            {
+                if (Chis > Znam)
+                {
+                    max = Convert.ToInt32(Chis);
+                    min = Convert.ToInt32(Znam);
+                }
+                else
+                {
+                    max = Convert.ToInt32(Znam);
+                    min = Convert.ToInt32(Chis);
+                }
+                while (max % min != 0)
+                {
+                    int tmp = max % min;
+                    max = min;
+                    min = tmp;
+                }
+                Chis /= min;
+                Znam /= min;
+            }
+        }
+
+        public static Fraction operator +(Fraction a)
+        {
+            return a;
+        }
+        public static Fraction operator -(Fraction a)
+        {
+            a.Chis = -a.Chis;
+            return a;
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Chis * b.Znam + a.Znam * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator +(Fraction a, int b)
+        {
+            return new Fraction(a.Chis * 1 + a.Znam * b, a.Znam);
+        }
+        public static Fraction operator +(int a, Fraction b)
+        {
+            return new Fraction(a * b.Znam + 1 * b.Chis, b.Znam);
+        }
+        public static Fraction operator +(Fraction a, double c)
+        {
+            Fraction b = new Fraction(c);
+            return new Fraction(a.Chis * b.Znam + a.Znam * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator +(double c, Fraction b)
+        {
+            Fraction a = new Fraction(c);
+            return new Fraction(a.Chis * b.Znam + a.Znam * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Chis * b.Znam - a.Znam * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator -(Fraction a, int b)
+        {
+            return new Fraction(a.Chis * 1 - a.Znam * b, a.Znam);
+        }
+        public static Fraction operator -(int a, Fraction b)
+        {
+            return new Fraction(a * b.Znam - 1 * b.Chis,b.Znam);
+        }
+        public static Fraction operator -(Fraction a, double c)
+        {
+            Fraction b = new Fraction(c);
+            return new Fraction(a.Chis * b.Znam - a.Znam * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator -(double c, Fraction b)
+        {
+            Fraction a = new Fraction(c);
+            return new Fraction(a.Chis * b.Znam - a.Znam * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Chis * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator *(Fraction a, int b)
+        {
+            return new Fraction(a.Chis * b, a.Znam);
+        }
+        public static Fraction operator *(int a, Fraction b)
+        {
+            return new Fraction(a * b.Chis, b.Znam);
+        }
+        public static Fraction operator *(Fraction a, double c)
+        {
+            Fraction b = new Fraction(c);
+            return new Fraction(a.Chis * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator *(double c, Fraction b)
+        {
+            Fraction a = new Fraction(c);
+            return new Fraction(a.Chis * b.Chis, a.Znam * b.Znam);
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return a * !b;
+        }
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            if (Convert.ToDouble(a) == Convert.ToDouble(b))
+                return true;
+            else return false;
+        }
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            if (Convert.ToDouble(a) != Convert.ToDouble(b))
+                return true;
+            else return false;
+        }
+        public static bool operator >(Fraction a, Fraction b)
+        {
+            if (Convert.ToDouble(a) > Convert.ToDouble(b))
+                return true;
+            else return false;
+        }
+        public static bool operator <(Fraction a, Fraction b)
+        {
+            if (Convert.ToDouble(a) < Convert.ToDouble(b))
+                return true;
+            else return false;
+        }
+        public static bool operator >=(Fraction a, Fraction b)
+        {
+            if (Convert.ToDouble(a) >= Convert.ToDouble(b))
+                return true;
+            else return false;
+        }
+        public static bool operator <=(Fraction a, Fraction b)
+        {
+            if (Convert.ToDouble(a) <= Convert.ToDouble(b))
+                return true;
+            else return false;
+        }
+
+
+        public static Fraction operator !(Fraction a)
+        {
+            Fraction b = new Fraction(a.Znam, a.Chis);
+            b.Fix();
+            return b;
+        }
+        public void Fix()
+        {
+            if ((Chis < 0 && Znam < 0) || (Chis > 0 && Znam < 0))
+            {
+                Chis = -Chis;
+                Znam = -Znam;
+            }
+        }
+        public static implicit operator double(Fraction a)
+        {
+            return Convert.ToDouble(a.Chis) / Convert.ToDouble(a.Znam);
+        }
+        public override string ToString()
+        {
+            return $"{Chis} / {Znam}";
+        }
+
+
+
+    }
+}
---
+++ oop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Class;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(2, 4);
+            Console.WriteLine("a = " + a.ToString());
+            Fraction b = new Fraction(-4, -8);
+            Console.WriteLine("b = " + b.ToString());
+            Fraction c = a * b;
+            Console.WriteLine("a * b = "+ c.ToString());
+            if (a == b)
+                Console.WriteLine("a = b");
+            Fraction d = new Fraction(43, 18);
+            Console.WriteLine("d = " + d.ToString());
+            Fraction e = new Fraction(78, -112);
+            Console.WriteLine("e = " + e.ToString());
+            Fraction f = new Fraction(12.568);
+            Console.WriteLine("f = " + f.ToString());
+            Fraction g = new Fraction(-61);
+            Console.WriteLine("g = " + g.ToString());
+            if (d < f)
+                Console.WriteLine("d < f");
+            Console.WriteLine("d * e / f + g = " + (d*e/f+g).ToString());
+        }
+    }
+}
